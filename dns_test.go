e4030c4ab7ebc2bb2645e7073a072d44cd2ca2ee
Miek Gieben <miek@miek.nl>
2022-07-19 15:33:45 +0200

    more
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- dns_test.go
+++ dns_test.go
@@ -14,6 +14,6 @@ func TestDNS(t *testing.T) {
 	rr.Hdr.TTL.Set(15)
 	rr.SetData(0, net.IPv4allrouter)
 
-	fmt.Printf("%s %s\n", rr.Hdr.String(), rr.String()) // example.net. 15 IN A	224.0.0.2
-	fmt.Printf("%#v %#v\n", rr.Hdr.GoString(), rr.String())
+	fmt.Printf("%s %s\n", rr.Hdr.String(), rr.String())     // example.net. 15 IN A	224.0.0.2
+	fmt.Printf("%#v %#v\n", rr.Hdr.GoString(), rr.String()) // "07example03net00 15 IN" "A\t224.0.0.2"
 }
