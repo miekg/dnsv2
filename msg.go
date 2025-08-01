89da8f188005530ecbf842cc252186ebddb942ce
Miek Gieben <miek@miek.nl>
2025-08-01 19:09:54 +0200

    Implemented reading RR header
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- msg.go
+++ msg.go
@@ -1,14 +1,13 @@
 package dns
 
-// Msg contains the layout of a DNS message.
-type Msg struct {
-	octets []byte
-}
-
 func (m *Msg) Octets(x ...[]byte) []byte {
 	if len(x) == 0 {
 		return m.octets
 	}
 	// TODO
 	return nil
+}
+
+func (m *Msg) Question(x ...Section) Section {
+	return Section{}
 }
--- types.go
+++ types.go
@@ -1,8 +1,13 @@
 package dns
 
-import "github.com/miekg/dnsv2/dnswire"
+import (
+	"encoding/binary"
+
+	"github.com/miekg/dnsv2/dnswire"
+)
 
 const (
+	ClassNone = dnswire.Class(0) // ClassNone signals a class not found condition.
 	// Valid classes in DNS, usually only ClassINET is used.
 	ClassINET   = dnswire.Class(1)
 	ClassCSNET  = dnswire.Class(2)
@@ -10,6 +15,12 @@ const (
 	ClassHESIOD = dnswire.Class(4)
 	ClassNONE   = dnswire.Class(254)
 	ClassANY    = dnswire.Class(255)
+)
+
+const (
+	TypeNone = dnswire.Type(0) // TypeNone signals a type not found condition.
+	// Valid DNS RR types.
+	TypeMX = dnswire.Type(15)
 )
 
 // Header is the header of an RR. All DNS resource records share this.
@@ -22,7 +33,74 @@ type Header interface {
 	Class(x ...dnswire.Class) dnswire.Class
 	// If Name does not have a parameter it returns the RR's owner name. If a parameter is given it sets the
 	// RR's owner name. Not that the name should not be compressed.
-	Name(x ...dnswire.Name) dnswire.Name
+	Name(x ...dnswire.Name) (dnswire.Name, error)
+}
+
+// header implements Header.
+type header struct {
+	octets []byte
+}
+
+func (h header) Type(x ...dnswire.Type) (dnswire.Type, error) {
+	name, err := h.Name()
+	if err != nil {
+		return TypeNone, err
+	}
+	if len(name)+2 < len(h.octets) {
+		return TypeNone, &Error{err: "overflow reading RR type"}
+	}
+	i := binary.BigEndian.Uint16(h.octets[len(name):])
+	return dnswire.Type(i), nil
+}
+
+func (h header) Class(x ...dnswire.Class) (dnswire.Class, error) {
+	name, err := h.Name()
+	if err != nil {
+		return ClassNone, err
+	}
+	if len(name)+4 < len(h.octets) {
+		return ClassNone, &Error{err: "overflow reading RR class"}
+	}
+	i := binary.BigEndian.Uint16(h.octets[len(name)+2:])
+	return dnswire.Class(i), nil
+}
+
+func (h header) TTL(x ...dnswire.TTL) (dnswire.TTL, error) {
+	name, err := h.Name()
+	if err != nil {
+		return dnswire.TTL(0), err
+	}
+	if len(name)+8 < len(h.octets) {
+		return dnswire.TTL(0), &Error{err: "overflow reading RR ttl"}
+	}
+	i := binary.BigEndian.Uint32(h.octets[len(name)+4:])
+	return dnswire.TTL(i), nil
+}
+
+func (h header) Name(x ...dnswire.Name) (dnswire.Name, error) {
+	off := 0
+	for {
+		c := int(h.octets[off])
+		off++
+		switch c & 0xC0 {
+		case 0x00:
+			if c == 0x00 {
+				// end of the name
+				return dnswire.Name(h.octets[:off]), nil
+			}
+			off += c
+		case 0xC0:
+			// Pointer to somewhere else in msg. We can't deal with that here because we don't have the message.
+			// Return error here.
+			return nil, ErrPointer
+		default:
+			// 0x80 and 0x40 are reserved
+			return nil, ErrLabelType
+		}
+		if off > len(h.octets) {
+			return nil, ErrBuf
+		}
+	}
 }
 
 // An RR represents a resource record.
@@ -31,4 +109,16 @@ type RR interface {
 	// If Octets does not have a parameter it returns the wire encoding octets for this RR. If a parameter is
 	// given the octect are written to the RR.
 	Octets(x ...[]byte) []byte
+}
+
+// Msg contains the layout of a DNS message. A DNS message has 4 sections, the question, answer, authority and additional section.
+// In this library _another_ section is added the pseudo section; this section contains EDNS0 "records" and a possible TSIG record.
+type Msg struct {
+	octets []byte
+}
+
+// Section is a section in a DNS message.
+type Section struct {
+	msg *Msg // msg is a pointer back the message this section belong in. This is needed to resolve compression pointers.
+	rrs []RR
 }
